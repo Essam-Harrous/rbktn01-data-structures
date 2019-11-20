cb4cafc084563fef9a2a1fcb2f53ffa53b567e65
essam <esamfarg91@gmail.com>
2019-11-20 19:52:14 +0100

    shared behavior

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+  	i: 0,
+  	storage: {}
+  };
+  return _.extend(instance, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(val) {
+		this.storage[this.i] = val;
+		this.i += 1;
+	},
+	dequeue: function() {
+		var poped;
+		if (this.storage[0] !== undefined) {
+			poped = this.storage[0];
+			delete this.storage[0];
+			this.storage = resort(this.storage);
+			this.i -= 1;
+			return poped;
+		}
+	},
+	size: function() {
+		return this.i;
+	}
+};
 
-
+var resort = function(obj) {
+	var result = {};
+	for (var i = 0; i < Object.values(obj).length; i++) {
+		result[i] = Object.values(obj)[i];
+	}
+	return result;
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someInstance = {
+		i: 0,
+		lastValue: '',
+		storage : {}
+	};
+	return _.extend(someInstance, stackMethods);
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.i] = value;
+    this.i += 1;
+  },
+  pop: function() {
+    if (this.i > 0) {	
+      this.i -= 1;
+      var poped = this.storage[this.i];
+			delete this.storage[this.i];
+			return poped;
+		}
+	},
+	size: function(){
+		return this.i;
+	}
+};
 
 
