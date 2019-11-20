9afcc837fa97aa3112fa35ea5684dd7232393a13
essam <esamfarg91@gmail.com>
2019-11-20 17:29:37 +0100

    stack and queue 1

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,38 @@
 var Queue = function() {
   var someInstance = {};
-
+  var i = 0;
+  var popped;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[i] = value;
+    i++;
   };
 
   someInstance.dequeue = function() {
+    console.log(i, storage);
+    if(storage[0] !== undefined){
+      popped = storage[0];
+      delete storage[0];
+      i--;  
+      storage = resort(storage);
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return i;
   };
 
   return someInstance;
+};
+
+var resort = function(obj){
+  var sortedObject = {};
+  for(var i = 0; i < Object.values(obj).length; i++){
+    sortedObject[i] = Object.values(obj)[i];
+  }
+  return sortedObject;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function() {
   var someInstance = {};
-
+  var i = 0;
+  var lastValue ;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[i] = value;
+    lastValue = value;
+    i++;
   };
 
   someInstance.pop = function() {
+    var popped = lastValue;
+    if(i > 0){
+      i--;
+      delete storage[i];
+      lastValue = storage[i - 1];
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return i;
   };
-
   return someInstance;
 };
